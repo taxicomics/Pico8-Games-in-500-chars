e51e774b50e4115aef33504813a3456ed8ad17ee
taxicomics <168220579+taxicomics@users.noreply.github.com>
2024-05-13 22:26:56 +0200

    Create StardewValleyFishingMinigame500.lua
    inital upload

---
+++ StardewValleyFishingMinigame500.lua
@@ -0,0 +1,14 @@
+n=0f=0i=0o=0d=0e=3r=61q="🐱"⌂=rnd l=sin function _draw()cls(1)if t()<r do if i~=0do rect(50,10,77,110)rect(55,15,67,105)rect(56,85-f,66,105-f)rect(69,105-d,75,105)?q,58,99-i
+i=mid(10,i+o,80)if(i>f and i<f+20)d+=2
+d=max(0,d-1)if(btn(❎))f=min(70,f+4)
+if(f>0)f-=2if(t()%█==0)o=⌂(3)-1.5
+if(d>89)n+=1
+if(d>89or d<1)d=9i=0e=t()+2
+else for f=0,127do pset(f,80+l(t()+f/8)*2)end?"♪",62,99
+if t()>e do if(btn(❎))i=1f=0
+?"❎",62,60+l(t()*5)
+if(t()>e+1)e=t()+3
+end end?q..n,1,1
+?r-t(),120,1
+else?n..q,62,62
+end end
